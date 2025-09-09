95a2c3b32497aa70e3942f047395c106908f2f88
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-09 10:47:11 +0530

    updated

--- userservice/src/main/java/com/boa/userservice/dtos/FullNameDTO.java
+++ userservice/src/main/java/com/boa/userservice/dtos/FullNameDTO.java
@@ -1,10 +1,14 @@
 package com.boa.userservice.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
@@ -12,11 +16,12 @@ import lombok.NoArgsConstructor;
 public class FullNameDTO {
     @NotNull
     @Pattern(regexp = "^[a-zA-Z]+$", message = "First Name must contain only alphabets")
+    @Schema(description = "First name value", example = "user1")
 
     private String firstName;
     @NotNull
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Last Name must contain only alphabets")
-
+    @Schema(description = "Last name value", example = "user2")
     private String lastName;
     private String midddleName;
 }
--- userservice/src/main/java/com/boa/userservice/models/User.java
+++ userservice/src/main/java/com/boa/userservice/models/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
