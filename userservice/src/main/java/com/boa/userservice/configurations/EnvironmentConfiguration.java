17a027104b3e8eaa374c6a9d21f35ffac523324c
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-09 15:10:08 +0530

    updated

--- userservice/src/main/java/com/boa/userservice/configurations/EnvironmentConfiguration.java
+++ userservice/src/main/java/com/boa/userservice/configurations/EnvironmentConfiguration.java
@@ -9,6 +9,6 @@ public class EnvironmentConfiguration {
 
     private String mysql_userName;
     private String password;
-    private String dirvername;
+    private String drivername;
     private String url;
 }
