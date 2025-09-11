a8f514181b79c95ccb497dc9de448d05bee0280f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-11 16:40:01 +0530

    updaed

---
+++ userservicepromgrafana/src/main/java/com/boa/userservice/configurations/VaultConfiguration.java
@@ -0,0 +1,12 @@
+package com.boa.userservice.configurations;
+/*
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties
+public class VaultConfiguration {
+    private String mysqlusername;
+    private String mysqlpassword;
+}
+*/
