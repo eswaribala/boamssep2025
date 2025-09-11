2c76193b1d093b8184e2b03ac9feca5c5f0cf57d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-11 11:37:09 +0530

    updated

---
+++ bofacloudapigateway/src/main/java/com/boa/bofacloudapigateway/BofacloudapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.boa.bofacloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BofacloudapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BofacloudapigatewayApplication.class, args);
+    }
+
+}
